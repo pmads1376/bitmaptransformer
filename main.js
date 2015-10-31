86d8e1a032c87ad9cbdea38285ecbc4f78bdc1c2
Peter Madsen <cephas.ryle@gmail.com>
2015-10-30 19:02:53 -0700

    fix error in transform for loop condition

--- main.js
+++ main.js
@@ -3,16 +3,14 @@ var fs = require('fs');
 fs.readFile("./testIn.bmp", transform);
 
 function transform(err, buffer) {
-  if(err){
-    throw err;
-  } else {
+  if(err)throw err; 
+  else {
+    var offset = buffer.readUInt16LE(10); // offset to the image in the buffer
+    var image = buffer.slice(offset, buffer.length);//get bytes
+    var length = image.length; //reduce lookup time in for loop by assigning length
 
-    var offset = buffer.readUInt16LE(10); // provides the offset to the image
-    var image = buffer.slice(offset, buffer.length);
-    var length = image.length;
-
-    for(var i = offset; i < length; i++){
-      buffer[i] = 0xFF - buffer[i];
+    for(var i = offset; i < buffer.length; i++){
+      buffer[i] = 0xFF- buffer[i]; //transform to flip image
     }
   
   fs.writeFile('./testOut.bmp', buffer, function(err){
