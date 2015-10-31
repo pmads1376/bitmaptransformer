5848b31903d5dbc7b5fb85a19070380291ccbe45
Peter Madsen <cephas.ryle@gmail.com>
2015-10-30 19:15:09 -0700

    fix gulpfile logic

--- gulpfile.js
+++ gulpfile.js
@@ -9,23 +9,10 @@ var gulp = require('gulp'),
       .pipe(jshint.reporter('default'));
   });
 
-  // gulp.task('style', function(){
-  //   return gulp.src('./*.js')
-  //     .pipe(jscs())
-  //     .pipe(jscs.reporter());
-  // });
-
-  // gulp.task('style-fixer', function() {
-  //   return gulp.src('./*.js')
-  //       .pipe(jscs({fix: true}))
-  //       .pipe(gulp.dest('.'));
-  // });
-
   gulp.task('test', function () {
     return gulp.src('test.js', {read: false})
       .pipe(mocha());
   });
 
   gulp.task('default', ['lint', 'test']);
-  // gulp.task('default', ['lint', 'test', 'style', 'style-fixer']);
-  // gulp.watch('./*.js', ['default']);
+  gulp.watch('./*.js', ['default']);
